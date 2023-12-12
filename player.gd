ac57ac61ec9225f24815001babab90c5c0b53551
MiDe-S <63002269+MiDe-S@users.noreply.github.com>
2023-12-11 23:49:08 -0500

    Added tilesets

--- player.gd
+++ player.gd
@@ -8,25 +8,46 @@ func getCritters():
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionX = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if directionX:
+		velocity.x = directionX * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+			
+	var directionY = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+		
+	if velocity == Vector2.ZERO:
+		$AnimationPlayer.stop()
+	elif velocity.x > 0:
+		$AnimationPlayer.play("right")
+	elif velocity.y < 0:
+		$AnimationPlayer.play("up")
+	elif velocity.x < 0:
+		$AnimationPlayer.play("left")
+	elif velocity.y > 0:
+		$AnimationPlayer.play("down")
+		
 	move_and_slide()
+
+func _input(event):
+	pass
+	#if event.is_action_pressed("move_down"):
+	#	$AnimatedSprite2D.play("down")
+	#elif event.is_action_pressed("move_up"):
+	#	$AnimatedSprite2D.play("up")
+	#elif event.is_action_pressed("move_left"):
+	#	$AnimatedSprite2D.play("left")
+	#elif event.is_action_pressed("move_right"):
+	#	$AnimatedSprite2D.play("right")
